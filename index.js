4268ec018a6ab1e992a7eff3179309ee4c7cbfbb
ayenyeinsan <aye2904@gmail.com>
2024-02-29 12:47:44 +0700

    added routes

--- index.js
+++ index.js
@@ -2,40 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const qbRoutes = require('./routes/qbroutes');
 const path = require('path');
-const dbModule = require('./config/mysqlconfig');
+
 
 const app = express();
 
 const port = 5001;
 
-// Apply body parsers before using the routes
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Use your routes after setting up the body parsers
-app.use(qbRoutes);
+app.use('/admin', qbRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-
-app.get('/category', async (req, res) => {
-    const sqlQuery = 'SELECT * FROM category';
-
-    try {
-        const pool = dbModule.createPool();
-        const connection = await pool.getConnection();
-        const [result, fields] = await connection.query(sqlQuery);
-        console.log(result);
-        res.json(result);
-    } catch (error) {
-        console.error('Error executing query:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    } 
-});
 app.listen(port, () => {
     console.log(`adminsite_backend is listening at ${port}`);
 });
--- routes/qbroutes.js
+++ routes/qbroutes.js
@@ -12,7 +12,7 @@ const Code = {
     OK: 200,
     CREATED: 201,
     INTERNAL_SERVER_ERROR: 500,
-    // Add other status codes as needed
+
   };
   const HttpResponse = class {
     constructor(code, status, message, data) {
@@ -25,10 +25,83 @@ const Code = {
   const Status = {
     OK: 200,
     INTERNAL_SERVER_ERROR: 500,
-    // Add other status codes as needed
+    
   };
+  Router.get('/billHistory', async (req, res) => {
+    const sqlQuery = 'SELECT * FROM bookstore.sale_History';
+  
+  
+    try {
+      const pool = dbModule.createPool();
+      const connection = await pool.getConnection();
+      const [result] = await connection.query(sqlQuery);
+  
+      const bills = result.map((row) => ({ id: row.billId, billDate: row.billDate , bookId: row.bookId ,quanity: row.quanity ,billPrice: row.billPrice }));
 
-  Router.get('/add',  async(req, res) => {
+      connection.release();
+  
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'All Bills', bills));
+      
+    } catch (err) {
+      console.error(err);
+      return res.status(Code.INTERNAL_SERVER_ERROR)
+        .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.ERROR, 'Internal Server Error'));
+    } 
+    
+  });
+
+  Router.get('/categories', async (req, res) => {
+    const sqlQuery = 'SELECT * FROM bookstore.category';
+  
+  
+    try {
+      const pool = dbModule.createPool();
+      const connection = await pool.getConnection();
+      const [result] = await connection.query(sqlQuery);
+  
+      const categories = result.map((row) => ({ id: row.categoryId, categoryName: row.categoryName }));
+
+      connection.release();
+  
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'All Categories', categories));
+      
+    } catch (err) {
+      console.error(err);
+      return res.status(Code.INTERNAL_SERVER_ERROR)
+        .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.ERROR, 'Internal Server Error'));
+    } 
+    
+  });
+  Router.get('/books', async (req, res) => {
+    const sqlQuery = 'SELECT * FROM bookstore.book';
+  
+  
+    try {
+      const pool = dbModule.createPool();
+      const connection = await pool.getConnection();
+      const [result] = await connection.query(sqlQuery);
+      const books = result.map((row) => ({ id: row.bookId, categoryId: row.categoryId ,coverImg: row.coverImg , title: row.title,
+       author : row.author, description : row.description , price : row.price , promotionPrice: row.promotion_price,
+      releasDate : row.releaseDate, publisher: row.publisher , language : row.language ,page: row.page , stock: row.stock,
+      salecount: row.sale_count }));
+
+      connection.release();
+  
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'All Books', books));
+      
+    } catch (err) {
+      console.error(err);
+      return res.status(Code.INTERNAL_SERVER_ERROR)
+        .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.ERROR, 'Internal Server Error'));
+    } 
+    
+  });
+  
+
+  Router.get('/addBook',  async(req, res) => {
     let book = [req.body];
     try {
         const pool = dbModule.createPool();
